1bf236ede0e11eccf23abbbaa5acbd7a5089179e
maximkorezkij <46990474+maximkorezkij@users.noreply.github.com>
2019-01-29 09:49:04 +0100

    first tasks as comments in main

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    return 0;
+}
+
+/* Books -
+ * ISBN- Nr
+ * title
+ * author
+ * number of books
+ * list of who borrowed books
+ */
+
+/* methods
+ * search books and show them
+ * borrow books
+ * add books
+ * delete books
+ */
+
+/* Additional:
+ * menu function
+ * books need to be saved in external file
+ *  most likely a text file where each row is a book
+ * read, write , open file functions
+ */
