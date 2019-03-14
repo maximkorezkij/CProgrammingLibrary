4df83595d76d4bb1de0cd5361ad7f394cd5135eb
zalhfira <47207394+zalhfira@users.noreply.github.com>
2019-03-14 18:58:27 +0100

    Revert "rent_book v1"
    This reverts commit 0fa0326d64db4281a33e440aaeb5deea929c90c4.

--- src/menu.c
+++ src/menu.c
@@ -161,7 +161,7 @@ void bookMenu(book *help) {
 
         switch (e) {
             case 1:
-                rentBook(help);
+                //rentBook
                 mainMenu();
                 break;
             case 2:
