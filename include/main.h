0c28b9f99e52b7d99dbda2a19f2f6c0908e27e76
zalhfira <47207394+zalhfira@users.noreply.github.com>
2019-03-14 08:46:47 +0100

    Create main.h

---
+++ include/main.h
@@ -0,0 +1,16 @@
+#define MAX 100
+
+typedef struct Book {       //ausleihliste fehlt noch
+    char *title;
+    char *author;
+    char *isbn_nr;
+    int nob;
+    char *r_list[][MAX];
+}book;
+
+typedef struct Lib {
+    book **Books;
+    int registered;
+}lib;
+
+lib lib1 = {};
